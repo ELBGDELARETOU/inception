f771dd17600d2a2b11e7316a038aaaba80a8652a
Adem Naouali <anaouali@bess-f4r1s8.clusters.42paris.fr>
2025-02-25 12:26:19 +0100

    lol

--- srcs/requirements/mariadb/mdb-conf.sh
+++ srcs/requirements/mariadb/mdb-conf.sh
@@ -3,6 +3,6 @@ sleep 5
 mariadb -e "create database if not exist\`${MYSQL_DB}\`;"
 mariadb -e "create user if not exist \`${MYSQL_USER}\`@'%' identified by '${MYSQL_PASSWORD}';"
 mariadb -e "grant all privileges on ${MYSQL_DB}. * to \`${MYSQL_USER}\`@'%';"
-mariadb -e "flush privileges"
-mysqladmin -u root -p$MYSQL_ROOT_PASSWORD sutdown
+mariadb -e "flush privileges;"
+mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
 mysqld_safe --port=3306 --bind-adress=0.0.0.0 --datadir='/var/lib/mysql'
